f559f476126983cb97912c347ef6b233c9bd8677
Irina <balkarovairina@gmail.com>
2021-08-21 13:51:03 +0200

    add: menu for UpdateFragment

--- app/src/main/java/ru/irinavb/todolist/fragments/add/AddFragment.kt
+++ app/src/main/java/ru/irinavb/todolist/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package ru.irinavb.todolist.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import ru.irinavb.todolist.R
 
 
@@ -14,6 +12,13 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 }

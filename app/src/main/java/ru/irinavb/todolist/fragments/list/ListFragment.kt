737fc495ed95d4c4c025bc5db4ec6fab6170605f
Irina <balkarovairina@gmail.com>
2021-08-21 13:40:44 +0200

    add: Menu for ListFragment

--- app/src/main/java/ru/irinavb/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/ru/irinavb/todolist/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package ru.irinavb.todolist.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import ru.irinavb.todolist.R
 import ru.irinavb.todolist.databinding.FragmentListBinding
@@ -33,6 +31,12 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }

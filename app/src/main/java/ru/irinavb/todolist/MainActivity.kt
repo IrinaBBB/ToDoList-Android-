b0f147f2416050d90dcd23d2bc8659636b257026
Irina <balkarovairina@gmail.com>
2021-08-21 09:47:11 +0200

    add: design for UpateFragment layout

--- app/src/main/java/ru/irinavb/todolist/MainActivity.kt
+++ app/src/main/java/ru/irinavb/todolist/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.irinavb.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import ru.irinavb.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setContentView(binding.root)
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/ru/irinavb/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/ru/irinavb/todolist/fragments/list/ListFragment.kt
@@ -5,14 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ru.irinavb.todolist.R
+import ru.irinavb.todolist.databinding.FragmentListBinding
+
+
+import androidx.navigation.fragment.NavHostFragment
+
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return binding.root
     }
 }

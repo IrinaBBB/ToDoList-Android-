920cf271cc275889984598d1dea992163a6b70bb
Irina <balkarovairina@gmail.com>
2021-08-22 13:35:36 +0200

    add: displaying data in RecyclerView

--- app/src/main/java/ru/irinavb/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/ru/irinavb/todolist/fragments/list/ListFragment.kt
@@ -2,19 +2,28 @@ package ru.irinavb.todolist.fragments.list
 
 import android.os.Bundle
 import android.view.*
+import android.widget.LinearLayout
+import android.widget.ListAdapter
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import ru.irinavb.todolist.R
 import ru.irinavb.todolist.databinding.FragmentListBinding
 
 
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.irinavb.todolist.viewmodels.ToDoViewModel
 
 
 class ListFragment : Fragment() {
 
     private lateinit var binding: FragmentListBinding
     private lateinit var navHostFragment: NavHostFragment
+    private val adapter: ToDoAdapter by lazy { ToDoAdapter() }
+    private val toDoViewModel: ToDoViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +31,13 @@ class ListFragment : Fragment() {
     ): View {
         binding = FragmentListBinding.inflate(inflater, container, false)
         navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        toDoViewModel.getAllData.observe(viewLifecycleOwner, Observer {
+            adapter.setData(it)
+        })
 
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
